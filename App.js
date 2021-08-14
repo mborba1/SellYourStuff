08bd50563420f11dc40da13b2a80debe635189b1
Margareth Borba <mborba1@icloud.com>
2021-08-14 09:49:28 -0400

    Front end styling and setup

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, ImageBackground} from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+
+import Card from './app/components/Card';
+import AppText from './app/components/AppText';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      // <WelcomeScreen />
+      <View style={{
+        backgroundColor: '#f8f4f4',
+        padding: 20,
+        paddingTop: 100,
+      }}>
+        <Card 
+        title="Yellow chair for sale"
+        subTitle='$100'
+        image={require("./app/assets/ron-mcclenny-9yI8eQ9mdvY-unsplash.jpg")}/>
+      </View>
+      
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
+    
   },
 });
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { ImageBackground, StyleSheet, View, Image} from 'react-native';
+import AppButton from '../components/AppButton';
+
+function WelcomeScreen(props) {
+    return (
+       <ImageBackground 
+            blurRadius={5}
+            style={styles.background}
+            source={require('../assets/ben-kolde-6lzIdGOoqfg-unsplash.jpg')}>
+             <Image style={styles.logo} source={require('../assets/sellyourstuff.png')}/> 
+             <View style={styles.buttonsContainer}>
+                <AppButton title="Login" />
+                <AppButton title="Register" color="secondary"/>
+             </View>        
+       </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        justifyContent:"flex-end",
+        alignItems: 'center'
+    },
+    buttonsContainer: {
+        padding: 20,
+        width: '100%',
+    },
+    logo: {
+        width: 200,
+        height: 200,
+        position: "absolute",
+        top: 70,
+        backgroundColor: '#f8f4f4',
+        
+
+    },
+
+})
+export default WelcomeScreen;
