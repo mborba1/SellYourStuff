989538dab650b8cadf3647a4d6dc0964eb352c34
Margareth Borba <mborba1@icloud.com>
2021-09-17 11:07:40 -0400

    added api call to listings

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
+import AuthNavigator from "./app/navigation/AuthNavigator";
 
 export default function App() {
   return (
@@ -12,5 +13,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-
--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,33 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { View , FlatList, StyleSheet } from 'react-native';
+
 import Card from '../components/Card';
 import routes from '../navigation/routes';
 import colors from '../config/colors';
+import listingsApi from '../api/listings'
 import Screen from '../components/Screen';
 
-const listings = [
-    {
-        id: 1,
-        title: 'Yellow chair for sale',
-        price: 100,
-        image: require("../assets/ron-mcclenny-9yI8eQ9mdvY-unsplash.jpg")
-    },
-    {
-        id: 2,
-        title: 'Couch in great conditions',
-        price: 1000,
-        image: require("../assets/paul-weaver-nWidMEQsnAQ-unsplash.jpg")
-    },
-    {
-        id: 3,
-        title: 'Men Leather Jacket',
-        price: 150,
-        image: require("../assets/dami-adebayo-k6aQzmIbR1s-unsplash.jpg")
-    },
+// const listings = [
+//     {
+//         id: 1,
+//         title: 'Yellow chair for sale',
+//         price: 100,
+//         image: require("../assets/ron-mcclenny-9yI8eQ9mdvY-unsplash.jpg")
+//     },
+//     {
+//         id: 2,
+//         title: 'Couch in great conditions',
+//         price: 1000,
+//         image: require("../assets/paul-weaver-nWidMEQsnAQ-unsplash.jpg")
+//     },
+//     {
+//         id: 3,
+//         title: 'Men Leather Jacket',
+//         price: 150,
+//         image: require("../assets/dami-adebayo-k6aQzmIbR1s-unsplash.jpg")
+//     },
 
-]
+// ]
 
 function ListingsScreen({ navigation }) {
+    const  [listings, setListings] = useState([]);
+
+    useEffect(() => { 
+        loadListings()    
+    }, []);
+
+    const loadListings= async () => {
+       const response = await listingsApi.getListings();
+       setListings(response.data);
+    }
+
     return (
         <View style={styles.screen}>
             <FlatList 
@@ -37,7 +50,7 @@ function ListingsScreen({ navigation }) {
                <Card 
                title={item.title}
                subTitle={`$${item.price}`}
-               image={item.image}
+               imageURL={item.images[0].url}
                onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
             />
             }
